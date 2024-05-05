0626b2aaa51df2353a87e4e84a7179dce8eacdbf
Piha Gupta <101244596+ncu-piha@users.noreply.github.com>
2024-05-05 13:38:50 +0530

    Add files via upload

---
+++ chat_app/lib/services/auth/login_or_register.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:minimal_chat_app/pages/login_page.dart';
+import 'package:minimal_chat_app/pages/register_page.dart';
+
+class LoginOrRegister extends StatefulWidget {
+  const LoginOrRegister({super.key});
+
+  @override
+  State<LoginOrRegister> createState() => _LoginOrRegisterState();
+}
+
+class _LoginOrRegisterState extends State<LoginOrRegister> {
+
+  //initially shoe thw login page
+  bool showLoginPage=true;
+
+  //toggle betwwn login and register page 
+  void togglePages(){
+    setState(() {
+      showLoginPage= !showLoginPage;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    if(showLoginPage){
+      return LoginPage(
+        onTap: togglePages,
+      );
+    }else{
+      return RegisterPage(
+        onTap: togglePages,
+      );
+    }
+  }
+}
